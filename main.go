1abb3455765cb4c8f5b23aa03ab11c23c1ae47ef
FrostyAqua37 <eivindc@uia.no>
2023-04-14 06:25:40 +0000

    Version 3.0

--- main.go
+++ main.go
@@ -40,28 +40,28 @@ func main() {
         }
         defer destinationFile.Close()
 
-        //Tar input fra bruker og gj  r om kommandoer basert p   input.
+        //Makes a new buffered scanner of input called "inputScanner".
         inputScanner := bufio.NewScanner(os.Stdin)
         fmt.Println("Type either \"convert\" to make a new file or \"average\" to get the average temperature.")
         inputScanner.Scan()
-        input := inputScanner.Text()
+        input := inputScanner.Text() //Takes the input and puts it in "input".
 
-        if (input == "convert" || input == "Convert") {
-		inputScanner02 := bufio.NewScanner(os.Stdin)
+        if (input == "convert" || input == "Convert") { //Checks if the input is either "convert" or "Convert"
+		inputScanner02 := bufio.NewScanner(os.Stdin) //New buffered scanner called "inputScanner02".
                 fmt.Println("Type either \"Celsius\" or \"Fahrenheit\" to chose output temperature.")
 			inputScanner02.Scan()
-			input02 := inputScanner02.Text()
+			input02 := inputScanner02.Text() //Takes input from "inputScanner02" and puts it in "input02".
 			var fahrenheit float64;
-				if (input02 == "Celsius" || input02 == "celsius" || input02 == "c" || input02 == "C") {
-					scanner := bufio.NewScanner(sourceFile)
-					for scanner.Scan() {
+				if (input02 == "Celsius" || input02 == "celsius" || input02 == "c" || input02 == "C") { //Checks if the input is one of the options listed.
+					scanner := bufio.NewScanner(sourceFile) //New scanner to "read" the sourcefile. 
+					for scanner.Scan() { //Loops through the file line by line and prints it out. .
 						line := scanner.Text()
-						items := strings.Split(line, ";")
+						items := strings.Split(line, ";") //Divides each line when a ";" appears in the line. 
 
 						fmt.Println(items)
 					}
 
-				}else if (input02 == "Fahrenheit" || input02 == "fahrenheit" || input02 == "f" || input02 == "F") {
+				}else if (input02 == "Fahrenheit" || input02 == "fahrenheit" || input02 == "f" || input02 == "F") { //Checks if the input is one of the options listed. 
 					scanner02 := bufio.NewScanner(sourceFile)
 					scanner02.Scan()
 
@@ -88,7 +88,14 @@ func main() {
 			input := inputScanner.Text()
 
 			if (input == "Celsius" || input == "celsius" || input =="c" || input == "C") {
-				fmt.Println("Test \"C\" passed.")
+				lineScanner := bufio.NewScanner(sourceFile)
+				lineCount := 0
+				for lineScanner.Scan() {
+					lineCount++
+				}
+				fmt.Println("Number of lines in file:", lineCount)
+
+				
 			}else if (input == "Fahrenheit" || input == "fahrenheit" || input == "f" || input == "F") {
 				fmt.Println("Test \"F\" passed.")
 			}else {
